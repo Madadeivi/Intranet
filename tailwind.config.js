9f4efe3a132a09a372246b154bd399cb9ce619db
David Alan Dorantes Torres <david.dorantes.torres@gmail.com>
2025-05-12 00:08:58 -0600

    Add Geometria font files and example HTML
    - Created example.html to demonstrate the usage of the Geometria font family.
    - Added style.css with @font-face declarations for various styles of the Geometria font, including normal, italic, thin, light, medium, bold, and heavy weights.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,18 @@
               primary: {
                 50:  '#f5fafc',
                 100: '#e4f1f9',
-                500: '#1e40af'
-              }
-            }
+                500: '#3472E5', // Pantone 3005U (hex)
+                900: '#353535', // Neutral Black U (hex)
+              },
+              secondary: {
+                500: '#1B3AED', // Pantone Blue 072U (hex)
+                600: '#0202AD', // 99-16 U (hex)
+              },
+              // Puedes agregar más tonos si lo necesitas
+            },
+            fontFamily: {
+              geometria: ['Geometria', 'sans-serif'],
+            },
           }
         },
         plugins: []
