80439c4071f1dc917f014f12066a545c6edae8a0
David Alan Dorantes Torres <david.dorantes.torres@gmail.com>
2025-05-15 17:41:55 -0600

    feat: agregar soporte para vite-imagetools y actualizar dependencias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { imagetools } from 'vite-imagetools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), imagetools()],
   base: process.env.VITE_BASE_PATH || '/',
 })
